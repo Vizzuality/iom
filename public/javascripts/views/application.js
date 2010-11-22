75da2033b6046aa476dfa30644a2f2933960f227
ra.barroso <ra.barroso@gmail.com>
2010-11-22 19:16:13 +0100

    organization finished

--- public/javascripts/views/application.js
+++ public/javascripts/views/application.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+
+	Cufon.set('fontFamily', 'Geneva').replace('.geneva');
+	Cufon.set('fontFamily', 'PT Sans').replace('.pt_sans');	
+	Cufon.set('fontFamily', 'Aller').replace('.aller');
+	
+	Cufon.replace('.pt_sans_bold', {
+		fontFamily: 'PT Sans',
+		fontWeight: 'bold'
+	});
 	
 
-}
+});
