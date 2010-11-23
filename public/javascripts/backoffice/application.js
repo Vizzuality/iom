bab97dfd791eeda0c6ed5209a2a5849bf173e78f
Javier Álvarez Medina <xavijam@gmail.com>
2010-11-23 19:50:39 +0100

    changes in donors

--- public/javascripts/backoffice/application.js
+++ public/javascripts/backoffice/application.js
@@ -1,6 +1,6 @@
 
   $(document).ready(function(ev){
     if ($('div.right.menu').length>0) {
-      $('div.right.menu').height($('div.med div.left').height());
+      $('div.right.menu').height($('div.block div.med div.left').height());
     }
   });
