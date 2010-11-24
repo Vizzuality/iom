2bfa58405a8dbdac72a85588dafe91394196855d
Javier Álvarez Medina <xavijam@gmail.com>
2010-11-24 15:56:17 +0100

    changes

--- public/javascripts/backoffice/application.js
+++ public/javascripts/backoffice/application.js
@@ -3,4 +3,16 @@
     if ($('div.right.menu').length>0) {
       $('div.right.menu').height($('div.block div.med div.left').height());
     }
+    
+    //if there is an error in some field
+    $('a.error').hover(
+      function() {
+        $(this).parent().find('div.error_msg').show();
+      }
+    );
+    $('div.error_msg p').hover(function(){},
+      function() {
+        $(this).parent().hide();
+      }
+    );
   });
