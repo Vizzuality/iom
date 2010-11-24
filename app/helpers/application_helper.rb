77335e756bf25bb93e98c2ec5392b06ccc7d5b31
Fernando Blat <ferblape@gmail.com>
2010-11-24 16:44:56 +0100

    Errores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
     raw("class=\"selected\"") if request.path == url_path
   end
 
+  def errors_for(obj, attribute)
+    return if action_name == 'new'
+    if obj.errors[attribute]
+      return raw(<<-HTML
+          <span class="field_error">
+            <a class="error"></a>
+            <div class="error_msg">
+              <p><span>#{obj.errors[attribute]}</span></p>
+            </div>
+          </span>
+      HTML
+)
+    end
+  end
+
 end
