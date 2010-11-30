cb114e342d94e8e714e1a474b69b5a4edc315f73
jatorre <jatorre@vizzuality.com>
2010-11-30 19:58:02 +0100

    sorted out the JS includes for the sites frontend

--- public/javascripts/sites/map.js
+++ public/javascripts/sites/map.js
@@ -1,3 +1,11 @@
 function initialize() {
-    first_map = new F1.Maker.Map({map_id: "37985", dom_id: "map"});
+    first_map = new F1.Maker.Map({
+        map_id: "37985", 
+        dom_id: "map",
+        uiLegend:false,
+        uiLayers:false,
+        onload: function() {
+            first_map.swf.addFilter(0, { expression: "$[city] = 'Atlanta'" }); 
+        }
+    });
 };
