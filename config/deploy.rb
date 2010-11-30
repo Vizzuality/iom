bd4f6a2f86c9c0b981bb505f4386fe4c682c5023
Fernando Blat <ferblape@gmail.com>
2010-11-30 17:59:16 +0100

    Deploy

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+require 'capistrano/ext/multistage'
+require 'config/boot'
+require 'hoptoad_notifier/capistrano'
+
+set :stages, %w(staging production)
+set :default_stage, "production"
+
+require "bundler/capistrano"
+
+default_run_options[:pty] = true
+
+set :application, 'iom'
+
+set :scm, :git
+# set :git_enable_submodules, 1
+set :git_shallow_clone, 1
+set :scm_user, 'ubuntu'
+set :repository, "git://github.com/Vizzuality/iom.git"
+ssh_options[:forward_agent] = true
+set :keep_releases, 5
+
+set :linode_staging, '178.79.131.104'
+set :linode_production, '178.79.142.149'
+set :user,  'ubuntu'
+
+set :deploy_to, "/home/ubuntu/www/#{application}"
+
+after  "deploy:update_code", :run_migrations, :symlinks, :asset_packages, :set_staging_flag
+after "deploy:update", "deploy:cleanup"
+
+desc "Restart Application"
+deploy.task :restart, :roles => [:app] do
+  run "touch #{current_path}/tmp/restart.txt"
+end
+
+desc "Migraciones"
+task :run_migrations, :roles => [:app] do
+  run <<-CMD
+    export RAILS_ENV=production &&
+    cd #{release_path} &&
+    rake db:migrate
+  CMD
+end
+
+task :symlinks, :roles => [:app] do
+  run <<-CMD
+    ln -s #{shared_path}/system #{release_path}/public/system;
+  CMD
+end
+
+desc 'Create asset packages'
+task :asset_packages, :roles => [:app] do
+ run <<-CMD
+   export RAILS_ENV=production &&
+   cd #{release_path} &&
+   rake asset:packager:build_all
+ CMD
+end
