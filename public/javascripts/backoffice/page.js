c95133981927ab1407a4e5065e40342f4bce3266
Javier Álvarez Medina <xavijam@gmail.com>
2010-12-01 14:08:53 +0100

    textarea

--- public/javascripts/backoffice/page.js
+++ public/javascripts/backoffice/page.js
@@ -1,24 +1,20 @@
 
     $(document).ready( function() {
-
-    
+      //Parent page combo
       $('span#parent_page').click(function(ev){
         ev.stopPropagation();
         ev.preventDefault();
-
         if (!$(this).hasClass('clicked')){
           $(this).addClass('clicked');
         }else {
           $(this).removeClass('clicked');
         }
-
         $(document).click(function(event) {
           if (!$(event.target).closest('span.parent_page').length) {
             $('span.parent_page.clicked').removeClass('clicked');
           };
         });
       });
-
       $('span#parent_page ul.options li').click(function(ev){
         ev.stopPropagation();
         ev.preventDefault();
@@ -32,6 +28,21 @@
           }
         });
       });
-
     
     });
+    
+    
+    
+    function limitChars(textid, limit, infodiv) {
+      var text = $('#'+textid).val(); 
+      var textlength = text.length;
+      if(textlength > limit) {
+        $('#' + infodiv).html(limit+' chars written');
+        $('#'+textid).val(text.substr(0,limit));
+        return false;
+      } else {
+        $('#' + infodiv).html((limit - textlength) +' chars left.');
+        $('#' + infodiv).css('color','#999999');
+        return true;
+      }
+    }
