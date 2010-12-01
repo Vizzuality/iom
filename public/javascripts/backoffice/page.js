e6f29d314af5293e66589a4de50057528ee11244
Javier Álvarez Medina <xavijam@gmail.com>
2010-12-01 13:01:01 +0100

    changes in pages of the site - combo styled

---
+++ public/javascripts/backoffice/page.js
@@ -0,0 +1,37 @@
+
+    $(document).ready( function() {
+
+    
+      $('span#parent_page').click(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        if (!$(this).hasClass('clicked')){
+          $(this).addClass('clicked');
+        }else {
+          $(this).removeClass('clicked');
+        }
+
+        $(document).click(function(event) {
+          if (!$(event.target).closest('span.parent_page').length) {
+            $('span.parent_page.clicked').removeClass('clicked');
+          };
+        });
+      });
+
+      $('span#parent_page ul.options li').click(function(ev){
+        ev.stopPropagation();
+        ev.preventDefault();
+        var id_ = $(this).children('a').attr('id');
+        $('span#parent_page').children('p').text($(this).children('a').text());
+        $('span#parent_page').children('p').attr('id',$(this).children('a').attr('id'));
+        $('span#parent_page.clicked').removeClass('clicked');
+        $('#page_parent_id option').each(function(index,element){
+          if ($(element).attr('value')==id_) {
+            $(element).attr('selected','selected');
+          }
+        });
+      });
+
+    
+    });
