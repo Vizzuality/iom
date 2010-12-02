c603dc9d10f3411db59792989d0b3ff5250f264f
Fernando Espinosa <fer@ferdev.com>
2010-12-02 12:34:38 +0100

    Removes default => false for published field in create_pages migration

--- db/migrate/20101122081057_create_pages.rb
+++ db/migrate/20101122081057_create_pages.rb
@@ -4,7 +4,7 @@ class CreatePages < ActiveRecord::Migration
       t.string    :title
       t.text      :body
       t.integer   :site_id
-      t.boolean   :published, :default => false
+      t.boolean   :published
       t.string    :permalink
       t.timestamps
     end
