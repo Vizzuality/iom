3648891c62ca919deb07fe0cfc0068e4ce13c25f
ra.barroso <ra.barroso@gmail.com>
2010-12-09 13:38:29 +0100

    Theme 1 imgs and style changed

--- public/javascripts/sites/application.js
+++ public/javascripts/sites/application.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
 
-	Cufon.set('fontFamily', 'Geneva').replace('.geneva');
-	Cufon.set('fontFamily', 'PT Sans').replace('.pt_sans');	
-	Cufon.set('fontFamily', 'Aller').replace('.aller');
-	
-	Cufon.replace('.pt_sans_bold', {
-		fontFamily: 'PT Sans',
-		fontWeight: 'bold'
-	});
-	
-
 });
