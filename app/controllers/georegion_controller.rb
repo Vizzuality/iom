b0ced5f1d77c10a839ef1ecff9c1d1a5af394e7a
jatorre <jatorre@vizzuality.com>
2010-12-20 19:38:30 +0100

    solved problems on the georegions controller

--- app/controllers/georegion_controller.rb
+++ app/controllers/georegion_controller.rb
@@ -4,20 +4,16 @@ class GeoregionController < ApplicationController
 
   def show
     if(request.url.match(/countries/))
-      puts "COUNTRIES"
-      
+      @area = Country.find(params[:id])
+      @area_parent = "America"
     else
-      puts "REGIONS"
-      
+      @area = Region.find(params[:id])
+      @area_parent = @area.country.try(:name)
     end
     
+    @projects = @area.projects.site(@site).where("id IN (#{@site.projects_ids.join(',')})").paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
     
-  end
 
-  def region
-    @region = Region.find(params[:id])
-    @projects = @region.projects.site(@site).where("id IN (#{@site.projects_ids.join(',')})").paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
-    
     respond_to do |format|
       format.html {render :show}
       format.js do
@@ -29,24 +25,6 @@ class GeoregionController < ApplicationController
       end
     end
     
-  end
-  
-  
-  def country
-    @country = Country.find(params[:id])
-    @projects = @country.projects.site(@site).where("projects.id IN (#{@site.projects_ids.join(',')})").paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
-    respond_to do |format|
-      format.html {render :show}
-      format.js do
-        render :update do |page|
-          page << "$('#projects_view_more').remove();"
-          page << "$('#projects').append('#{escape_javascript(render(:partial => 'projects/projects'))}');"
-          page << "IOM.ajax_pagination();"
-        end
-      end
-    end
-    
-    render :show
   end
 
 end
