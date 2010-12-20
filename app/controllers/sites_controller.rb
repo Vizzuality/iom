cbe9cc117b8c2a5163090957e5303e67c5625152
jatorre <jatorre@vizzuality.com>
2010-12-20 19:04:02 +0100

    Removed region/country resources and work on a generic page for any 'region'

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -23,14 +23,14 @@ class SitesController < ApplicationController
     # Get the data for the map depending on the region definition of the site (country or region)
     if(@site.geographic_context_country_id)
       sql="select r.id,count(ps.project_id) as count,r.name,x(ST_Centroid(r.the_geom)) as lon,
-          y(ST_Centroid(r.the_geom)) as lat,r.name,'/region/'||r.id as url
+          y(ST_Centroid(r.the_geom)) as lat,r.name,'/regions/'||r.id as url
           from ((projects_regions as pr inner join projects_sites as ps on pr.project_id=ps.project_id and ps.site_id=#{@site.id}) 
           inner join regions as r on pr.region_id=r.id and r.level=3)
           inner join countries as c on r.country_id=c.id
           group by r.id,r.name,lon,lat,c.name,url"
     else
       sql="select c.id,count(ps.project_id) as count,c.name,x(ST_Centroid(c.the_geom)) as lon,y(ST_Centroid(c.the_geom)) as lat,
-          '/country/'||c.id as url
+          '/countries/'||c.id as url
           from (countries_projects as cp inner join projects_sites as ps on cp.project_id=ps.project_id and site_id=#{@site.id}) 
           inner join countries as c on cp.country_id=c.id
           group by c.id,c.name,lon,lat"
