5bca81a6cc79c9bdc64b61f176e3851a717671d2
Fernando Blat <ferblape@gmail.com>
2010-12-21 17:07:19 +0100

    Setting the right size of logo's and media resources thumbnails

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,21 +3,28 @@
 # Table name: partners
 #
 #  id                :integer         not null, primary key
-#  site_id           :integer         
-#  name              :string(255)     
-#  url               :string(255)     
-#  logo_file_name    :string(255)     
-#  logo_content_type :string(255)     
-#  logo_file_size    :integer         
-#  logo_updated_at   :datetime        
-#  created_at        :datetime        
-#  updated_at        :datetime        
+#  site_id           :integer
+#  name              :string(255)
+#  url               :string(255)
+#  logo_file_name    :string(255)
+#  logo_content_type :string(255)
+#  logo_file_size    :integer
+#  logo_updated_at   :datetime
+#  created_at        :datetime
+#  updated_at        :datetime
 #
 
 class Partner < ActiveRecord::Base
 
   belongs_to :site
-  has_attached_file :logo, :styles => { :small => "60x60#" }
+  has_attached_file :logo, :styles => {
+                                      :small => {
+                                        :geometry => "80x46#",
+                                        :quality => 90,
+                                        :format => 'jpg'
+                                      }
+                                    },
+                            :url => "/system/:attachment/:id/:style.:extension"
 
   validates_presence_of :name
 
--- app/models/site.rb
+++ app/models/site.rb
@@ -3,45 +3,45 @@
 # Table name: sites
 #
 #  id                              :integer         not null, primary key
-#  name                            :string(255)     
-#  short_description               :text            
-#  long_description                :text            
-#  contact_email                   :string(255)     
-#  contact_person                  :string(255)     
-#  url                             :string(255)     
-#  permalink                       :string(255)     
-#  google_analytics_id             :string(255)     
-#  logo_file_name                  :string(255)     
-#  logo_content_type               :string(255)     
-#  logo_file_size                  :integer         
-#  logo_updated_at                 :datetime        
-#  theme_id                        :integer         
-#  blog_url                        :string(255)     
-#  word_for_clusters               :string(255)     
-#  word_for_regions                :string(255)     
-#  show_global_donations_raises    :boolean         
+#  name                            :string(255)
+#  short_description               :text
+#  long_description                :text
+#  contact_email                   :string(255)
+#  contact_person                  :string(255)
+#  url                             :string(255)
+#  permalink                       :string(255)
+#  google_analytics_id             :string(255)
+#  logo_file_name                  :string(255)
+#  logo_content_type               :string(255)
+#  logo_file_size                  :integer
+#  logo_updated_at                 :datetime
+#  theme_id                        :integer
+#  blog_url                        :string(255)
+#  word_for_clusters               :string(255)
+#  word_for_regions                :string(255)
+#  show_global_donations_raises    :boolean
 #  project_classification          :integer         default(0)
-#  geographic_context_country_id   :integer         
-#  geographic_context_region_id    :integer         
-#  project_context_cluster_id      :integer         
-#  project_context_sector_id       :integer         
-#  project_context_organization_id :integer         
-#  project_context_tags            :string(255)     
-#  created_at                      :datetime        
-#  updated_at                      :datetime        
-#  project_context_tags_ids        :string(255)     
-#  status                          :boolean         
+#  geographic_context_country_id   :integer
+#  geographic_context_region_id    :integer
+#  project_context_cluster_id      :integer
+#  project_context_sector_id       :integer
+#  project_context_organization_id :integer
+#  project_context_tags            :string(255)
+#  created_at                      :datetime
+#  updated_at                      :datetime
+#  project_context_tags_ids        :string(255)
+#  status                          :boolean
 #  visits                          :float           default(0.0)
 #  visits_last_week                :float           default(0.0)
-#  aid_map_image_file_name         :string(255)     
-#  aid_map_image_content_type      :string(255)     
-#  aid_map_image_file_size         :integer         
-#  aid_map_image_updated_at        :datetime        
-#  geographic_context_geometry     :string          
-#  overview_map_bbox_miny          :float           
-#  overview_map_bbox_minx          :float           
-#  overview_map_bbox_maxy          :float           
-#  overview_map_bbox_maxx          :float           
+#  aid_map_image_file_name         :string(255)
+#  aid_map_image_content_type      :string(255)
+#  aid_map_image_file_size         :integer
+#  aid_map_image_updated_at        :datetime
+#  geographic_context_geometry     :string
+#  overview_map_bbox_miny          :float
+#  overview_map_bbox_minx          :float
+#  overview_map_bbox_maxy          :float
+#  overview_map_bbox_maxx          :float
 #
 
 class Site < ActiveRecord::Base
@@ -61,7 +61,15 @@ class Site < ActiveRecord::Base
   belongs_to :geographic_context_country, :class_name => 'Country'
   belongs_to :geographic_context_region, :class_name => 'Region'
 
-  has_attached_file :logo, :styles => { :small => "60x60#" }
+  has_attached_file :logo, :styles => {
+                                      :small => {
+                                        :geometry => "80x46#",
+                                        :quality => 90,
+                                        :format => 'jpg'
+                                      }
+                                    },
+                            :url => "/system/:attachment/:id/:style.:extension"
+
   has_attached_file :aid_map_image, :styles => {
                                       :small => {
                                         :geometry => "203x115#",
@@ -482,5 +490,5 @@ class Site < ActiveRecord::Base
       extension = File.extname(aid_map_image_file_name)
       self.aid_map_image_file_name = "#{aid_map_image_file_name.sanitize}#{extension}"
     end
-  
+
 end
