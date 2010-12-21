9b1c691213ee21363b18981185ad3c2cc4cdb86b
ra.barroso <ra.barroso@gmail.com>
2010-12-21 11:43:01 +0100

    conflicts

--- public/javascripts/sites/pages.js
+++ public/javascripts/sites/pages.js
@@ -12,6 +12,22 @@
         }
         
         
+        //remove text from input
+        $('input.main_search').focusin(function(ev){
+          var value = $(this).attr('value');
+          if (value == "Search donors by name, place,..." || value == "Search Organizations by name, place,..." || value == "Search projects by name, place,..." || value == "Search sites by name, place,...") {
+            old_value = value;
+            $(this).attr('value','');
+          }
+        });
+        $('input.main_search').focusout(function(ev){
+          var value = $(this).attr('value');
+          if (value == "") {
+            $(this).attr('value',old_value);
+          }
+        });
+        
+        
         // $('ul.filter_list').each(function(element){
         //   if ($(element).children('li').size()>10) {
         //     var compress_list = $(element);
