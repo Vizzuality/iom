bc1d8ceae75db9e82f7cbd06c846363a863554a6
jatorre <jatorre@vizzuality.com>
2010-12-22 13:02:13 +0100

    new map definitions on JS

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,35 @@ class ProjectsController < ApplicationController
     unless @project = @site.projects.select{ |p| p.id == params[:id].to_i}.first
       raise ActiveRecord::RecordNotFound
     end
+    
+    #Map data
+    sql="select r.id,x(ST_Centroid(r.the_geom)) as lon,y(ST_Centroid(r.the_geom)) as lat,r.name,r.code
+    from (projects as p inner join projects_regions as pr on pr.project_id=p.id and p.id=1) 
+    inner join regions as r on pr.region_id=r.id and r.level=1"
+    
+    result=ActiveRecord::Base.connection.execute(sql)
+    @map_data=result.to_json
+    @overview_map_bbox = [{
+              :lat => @site.overview_map_bbox_miny,
+              :lon => @site.overview_map_bbox_minx}, {
+              :lat => @site.overview_map_bbox_maxy,
+              :lon => @site.overview_map_bbox_maxx}]
+    @overview_map_chco = @site.theme.data[:overview_map_chco]
+    @overview_map_chf = @site.theme.data[:overview_map_chf]
+    @overview_map_marker_source = @site.theme.data[:overview_map_marker_source]
+
+    areas= []
+    data = []
+    @map_data_max_count=0
+    result.each do |c|
+      areas << c["code"]
+      data  << 1
+    end
+    @chld = areas.join("|")
+    @chd  = "t:"+data.join(",")    
+    
+    
+    
     respond_with(@project)
   end
 
