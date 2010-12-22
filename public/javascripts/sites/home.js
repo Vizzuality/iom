c4ff92e1f76226bfb41cd6922dd51c7962c8d0e2
Javier Álvarez Medina <xavijam@gmail.com>
2010-12-22 13:54:10 +0100

    head home fixed

--- public/javascripts/sites/home.js
+++ public/javascripts/sites/home.js
@@ -28,12 +28,11 @@
       
       // TO ADAPT LEFT AND RIGHT COLUMN PROPERLY
       if ($('div#left_column div.outer_float').height() > $('div#left_column div.right').height()){
-          $('div#left_column div.right').height($('div#left_column div.outer_float').height());
-      }else {
-          $('div#left_column div.outer_float').height($('div#left_column div.right').height());
-          var dif = $('div#left_column div.outer_float').height() - ($('div#left_column div.float_left').height());
-          $('div#most_active_orgs').height($('div#most_active_orgs').height() + dif + 41);
-          
+        $('div#left_column div.right').height($('div#left_column div.outer_float').height());
+      } else {
+        $('div#left_column div.outer_float').height($('div#left_column div.right').height());
+        var dif = $('div#left_column div.outer_float').height() - ($('div#left_column div.float_left').height());
+        $('div#most_active_orgs').height($('div#most_active_orgs').height() + dif + 41);
       }
 
     });
