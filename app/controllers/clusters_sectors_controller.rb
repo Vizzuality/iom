1971e8e28e2d68904ee02a70cd0670d94b0e5735
Fernando Blat <ferblape@gmail.com>
2010-12-22 18:41:08 +0100

    Don't allow to navigate by sector nor cluster depending on the configuration of the site

--- app/controllers/clusters_sectors_controller.rb
+++ app/controllers/clusters_sectors_controller.rb
@@ -3,17 +3,19 @@ class ClustersSectorsController < ApplicationController
   layout 'site_layout'
 
   def show
-    
+
     if(request.url.match(/clusters/))
+      render_404 and return unless @site.navigate_by_cluster?
       #CLUSTERS
       @data = Cluster.find(params[:id])
       @projects = @data.projects.where("projects.id IN (#{@site.projects_ids.join(',')})").paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
     else
       #SECTORS
+      render_404 and return unless @site.navigate_by_sector?
       @data = Sector.find(params[:id])
       @projects = @data.projects.where("projects.id IN (#{@site.projects_ids.join(',')})").paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
     end
-    
+
     respond_to do |format|
       format.html
       format.js do
