1b2b049f714a730eb6f597926bb796d8ceab9130
Fernando Blat <ferblape@gmail.com>
2010-12-24 15:21:41 +0100

    Donor pagination

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -7,6 +7,18 @@ class DonorsController < ApplicationController
     @donor.attributes = @donor.attributes_for_site(@site)
 
     @projects = Project.custom_find(@site, :donor_id => @donor.id, :per_page => 10, :page => params[:page], :order => 'created_at DESC')
+
+    respond_to do |format|
+      format.html
+      format.js do
+        render :update do |page|
+          page << "$('#projects_view_more').remove();"
+          page << "$('#projects').append('#{escape_javascript(render(:partial => 'donors/projects'))}');"
+          page << "IOM.ajax_pagination();"
+          page << "resizeColumn();"
+        end
+      end
+    end
   end
 
 end
