bb69731bd2b38ec414655f86490c606e695a4d68
Fernando Blat <ferblape@gmail.com>
2011-01-05 17:24:00 +0100

    If we are in the main level whe only show the projects of this level

--- app/controllers/georegion_controller.rb
+++ app/controllers/georegion_controller.rb
@@ -18,6 +18,7 @@ class GeoregionController < ApplicationController
 
     if geo_ids.size == 1
       @projects = Project.custom_find @site, :country => country.name,
+                                             :level => 1,
                                              :per_page => 10,
                                              :page => params[:page],
                                              :order => 'created_at DESC',
@@ -58,14 +59,27 @@ class GeoregionController < ApplicationController
 
       @area_parent = country.name
 
-      sql="select *,(select the_geom_geojson from regions where id=subq.id) as geojson
-        from(
-        select r.id,count(ps.project_id) as count,r.name,r.center_lon as lon,r.center_lat as lat
-        from (projects_regions as pr
-          inner join projects_sites as ps on pr.project_id=ps.project_id and site_id=#{@site.id})
-          inner join projects as p on ps.project_id=p.id and (p.end_date is null OR p.end_date > now())
-          inner join regions as r on pr.region_id=r.id and r.id=#{@area.id}
-        group by r.id,r.name,lon,lat) as subq"
+      # If we are in the main level whe only show the projects of 
+      # this level
+      if @area.level == @site.levels_for_region.max
+        sql="select *,(select the_geom_geojson from regions where id=subq.id) as geojson
+          from(
+          select r.id,count(ps.project_id) as count,r.name,r.center_lon as lon,r.center_lat as lat
+          from (projects_regions as pr
+            inner join projects_sites as ps on pr.project_id=ps.project_id and site_id=#{@site.id})
+            inner join projects as p on ps.project_id=p.id and (p.end_date is null OR p.end_date > now())
+            inner join regions as r on pr.region_id=r.id and r.id=#{@area.id} and r.level=#{@area.level}
+          group by r.id,r.name,lon,lat) as subq"
+      else
+        sql="select *,(select the_geom_geojson from regions where id=subq.id) as geojson
+          from(
+          select r.id,count(ps.project_id) as count,r.name,r.center_lon as lon,r.center_lat as lat
+          from (projects_regions as pr
+            inner join projects_sites as ps on pr.project_id=ps.project_id and site_id=#{@site.id})
+            inner join projects as p on ps.project_id=p.id and (p.end_date is null OR p.end_date > now())
+            inner join regions as r on pr.region_id=r.id and r.parent_region_id=#{@area.id} and r.level=#{@area.level+1}
+          group by r.id,r.name,lon,lat) as subq"
+      end
     end
 
     render_404 and return if sql.nil?
