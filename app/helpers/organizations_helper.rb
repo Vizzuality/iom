3a9f6636d7490722863bd39c2c256d5ec2534d94
Fernando Blat <ferblape@gmail.com>
2011-01-07 16:14:55 +0100

    Funding Information of the Organization

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,14 @@
 module OrganizationsHelper
+
+  def funding_percentages(organization)
+    private_percentage = ((organization.private_funding_spent / organization.budget)*100).to_i rescue 0
+    usg_percentage = ((organization.usg_funding_spent / organization.budget)*100).to_i rescue 0
+    other_percentages = 100 - private_percentage - usg_percentage
+    result = []
+    result << "#{private_percentage}% private" if private_percentage > 0
+    result << "#{usg_percentage}% USG" if usg_percentage > 0
+    result << "#{other_percentages}% other" if other_percentages > 0
+    result.join(', ')
+  end
+
 end
