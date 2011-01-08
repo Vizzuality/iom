a08fbb41b7ed788e6a2087d95db553075f028985
Fernando Blat <ferblape@gmail.com>
2011-01-08 10:11:06 +0100

    Bugfix: organization map in Food Security

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,7 +23,7 @@ class OrganizationsController < ApplicationController
         if(@site.geographic_context_country_id)
           sql="select r.id,count(ps.project_id) as count,r.name,r.center_lon as lon,r.center_lat as lat,r.name,'/regions/'||r.id as url,r.code
                 from ((((projects as p inner join organizations as o on o.id=p.primary_organization_id and
-                o.id=#{params[:id].gsub(/\\/, '\&\&').gsub(/'/, "''")})
+                o.id=#{params[:id].sanitize_sql!})
                 inner join projects_sites as ps on p.id=ps.project_id and ps.site_id=#{@site.id})
                 inner join projects as prj on ps.project_id=prj.id and (prj.end_date is null OR prj.end_date > now())
                 inner join projects_regions as pr on pr.project_id=p.id)
@@ -31,11 +31,11 @@ class OrganizationsController < ApplicationController
                 group by r.id,r.name,lon,lat,r.name,url,r.code"
         else
           sql="select c.id,count(ps.project_id) as count,c.name,r.center_lon as lon,r.center_lat as lat,c.name,'/location/'||c.id as url,c.iso2_code as code
-                from ((((projects as p inner join organizations as o on o.id=p.primary_organization_id and o.id=
-                #{params[:id].gsub(/\\/, '\&\&').gsub(/'/, "''")})
+                from ((((projects as p inner join organizations as o on o.id=p.primary_organization_id and o.id=#{params[:id].sanitize_sql!})
                 inner join projects_sites as ps on p.id=ps.project_id and ps.site_id=#{@site.id}) inner join projects_regions as pr on pr.project_id=p.id)
                 inner join projects as prj on ps.project_id=prj.id and (prj.end_date is null OR prj.end_date > now())
                 inner join countries_projects as cp on cp.project_id=ps.project_id)
+                inner join regions as r on pr.region_id=r.id and r.level=#{@site.level_for_region}
                 inner join countries as c on cp.country_id=c.id
                 group by c.id,c.name,lon,lat,c.name,url,c.iso2_code"
         end
