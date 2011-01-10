5357ed83a6bb9e868cea3b4002e4b5d6cd93f5df
Javier Álvarez Medina <xavijam@gmail.com>
2011-01-10 17:00:24 +0100

    200 ie6 page pushed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
 
   def old_browser
     render :file => "/public/old_browser.html.erb", :status => 200, :layout => false
-    render :file => "public/404.html.erb", :status => 404, :layout => false
   end
 
   protected
@@ -84,7 +83,7 @@ class ApplicationController < ActionController::Base
 
     def browser_is_ie6_or_lower?
       user_agent = request.user_agent.downcase
-      if ie_version = user_agent.match(/MSIE (\d)\.\d;/)
+      if ie_version = user_agent.match(/msie (\d)\.\d;/)
         ie_version = ie_version[1].to_i if ie_version && ie_version.size > 1 && ie_version[1]
         if ie_version && ie_version <= 6
           raise BrowserIsIE6OrLower
