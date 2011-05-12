515f572b862c4119e63f8c22f99ab4b9bf6345c1
Fernando Espinosa <fer@ferdev.com>
2011-05-12 14:42:23 +0200

    Fixes footer in main site's about page

--- public/javascripts/sites/home_pages.js
+++ public/javascripts/sites/home_pages.js
@@ -2,7 +2,7 @@
 
   $(document).ready( function() {
       if ($('div#footer_sites').length > 0){
-        var dif_height = $('div.body').height() - $('div.left').height();
+        var dif_height = $('div.body').outerHeight() - $('div.left').outerHeight();
         $('div#footer_sites').css('padding-top',dif_height - 40);
       }
   });
