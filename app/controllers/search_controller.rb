cc484abeb5526891c3ce38e9b8f2ac624bbf102b
Fernando Espinosa <fer@ferdev.com>
2011-06-14 17:01:50 +0200

    Fixes the query which gets the regions, sectors, organizations and donors in the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -102,6 +102,7 @@ class SearchController < ApplicationController
 
     respond_to do |format|
       format.html do
+        q_filter = q.present?? "AND (p.name ilike '#{q}' OR p.description ilike '#{q}')" : ''
         # cluster / sector Facet
         if @site.navigate_by_cluster?
           sql = <<-SQL
@@ -109,7 +110,7 @@ class SearchController < ApplicationController
             FROM clusters AS c
             INNER JOIN clusters_projects AS cp ON c.id=cp.cluster_id
             INNER JOIN projects_sites AS ps ON cp.project_id=ps.project_id AND ps.site_id=#{@site.id}
-            INNER JOIN projects AS p ON ps.project_id=p.id AND (p.end_date is NULL OR p.end_date > now())
+            INNER JOIN projects AS p ON ps.project_id=p.id AND (p.end_date is NULL OR p.end_date > now()) #{q_filter}
             #{filtered_clusters_where}
           SQL
           @clusters = Cluster.find_by_sql(sql)
@@ -119,7 +120,7 @@ class SearchController < ApplicationController
             FROM sectors AS s
             INNER JOIN projects_sectors AS ps ON s.id=ps.sector_id
             INNER JOIN projects_sites AS psi ON psi.project_id=ps.project_id AND psi.site_id=#{@site.id}
-            INNER JOIN projects AS p ON psi.project_id=p.id AND (p.end_date is NULL OR p.end_date > now())
+            INNER JOIN projects AS p ON psi.project_id=p.id AND (p.end_date is NULL OR p.end_date > now()) #{q_filter}
             #{filtered_sectors_where}
           SQL
           @sectors = Sector.find_by_sql(sql)
@@ -130,7 +131,7 @@ class SearchController < ApplicationController
           FROM regions AS r
           INNER JOIN projects_regions AS pr ON r.id=pr.region_id AND r.level=#{@site.level_for_region}
           INNER JOIN projects_sites AS ps ON pr.project_id=ps.project_id AND ps.site_id=#{@site.id}
-          INNER JOIN projects AS p ON ps.project_id=p.id AND (p.end_date is NULL OR p.end_date > now()) AND (p.name ilike '#{q}' OR p.description ilike '#{q}')
+          INNER JOIN projects AS p ON ps.project_id=p.id AND (p.end_date is NULL OR p.end_date > now()) #{q_filter}
           INNER JOIN countries AS c ON c.id = r.country_id
           #{filtered_regions_where}
         SQL
@@ -139,7 +140,7 @@ class SearchController < ApplicationController
         sql = <<-SQL
           SELECT DISTINCT o.id, o.name AS title
           FROM organizations AS o
-          INNER JOIN projects AS p ON (p.end_date is NULL OR p.end_date > now()) AND (p.name ilike '#{q}' OR p.description ilike '#{q}') AND p.primary_organization_id = o.id
+          INNER JOIN projects AS p ON (p.end_date is NULL OR p.end_date > now()) AND p.primary_organization_id = o.id #{q_filter}
           INNER JOIN projects_sites AS ps ON ps.project_id = p.id AND ps.site_id = #{@site.id}
           #{filtered_organizations_where}
         SQL
@@ -148,7 +149,7 @@ class SearchController < ApplicationController
         sql = <<-SQL
           SELECT DISTINCT d.id, d.name AS title
           FROM donors AS d
-          INNER JOIN projects AS p ON (p.end_date is NULL OR p.end_date > now()) AND (p.name ilike '#{q}' OR p.description ilike '#{q}')
+          INNER JOIN projects AS p ON (p.end_date is NULL OR p.end_date > now()) #{q_filter}
           INNER JOIN projects_sites AS ps ON ps.project_id = p.id AND ps.site_id = #{@site.id}
           INNER JOIN donations AS dn ON dn.donor_id = d.id AND dn.project_id = p.id
           #{filtered_donors_where}
