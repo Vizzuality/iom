146f8a5d2c4068ee7e0afbc884ec9fc675225b23
Fernando Espinosa <fer@ferdev.com>
2011-06-20 17:51:38 +0200

    Replaces semicolon with comma as field separator in csv exports

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -1,7 +1,7 @@
 module CsvMethods
   def serialize_to_csv(options = {})
     return '' unless self.any?
-    FasterCSV.generate(:col_sep => ';') do |csv|
+    FasterCSV.generate do |csv|
       csv << options[:headers]
       self.each do |item|
         csv << (item.csv_columns rescue item.values)
