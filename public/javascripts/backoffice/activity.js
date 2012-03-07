8fc3c957501068f08d1354043db0a8add2b58900
Javier Alvarez <xavijam@gmail.com>
2012-03-07 18:53:17 +0100

    Fixes onchange

--- public/javascripts/backoffice/activity.js
+++ public/javascripts/backoffice/activity.js
@@ -1,11 +1,26 @@
 $(document).ready(function(ev){
 
-  var hasChanged = false;
+  var hasChanged = [];
+
+  hasChanged['start'] = false; 
+  hasChanged['end']   = false;
+
+  function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+  }
+
+  function allSelected(kind) {
+    var month =  !isBlank($("#search_when_"+kind+"_2i_chzn span").html());
+    var day   =  !isBlank($("#search_when_"+kind+"_3i_chzn span").html());
+    var year  =  !isBlank($("#search_when_"+kind+"_1i_chzn span").html());
+
+    console.log(kind, month && day && year);
+    return month && day && year;
+  }
 
   $('.chzn-container').click(function(ev){
     ev.stopPropagation();
     ev.preventDefault();
-
   });
 
   $('span.combo_date').click(function(ev){
@@ -16,34 +31,67 @@ $(document).ready(function(ev){
 
     if ($('span.combo_date').hasClass('clicked')) {
 
-      $('span.combo_date:not(.open)').removeClass('clicked');
+      var id   = $('span.combo_date.open select').attr('id');
+      var kind = (id.indexOf("end") != -1) ? "end" : "start";
 
-      if (hasChanged) {
+      $('span.combo_date:not(.open)').removeClass('clicked');
+      
+      console.log("Checking " + kind);
+
+      if (hasChanged[kind] && allSelected(kind)) {
         $('form').closest('form').submit();
-        hasChanged = false;
+        hasChanged[kind] = false;
       }
     }
 
     $(this).toggleClass('clicked');
     $(this).removeClass('open');
+
   });
 
   $(document).click(function(event) {
-    if ( $('span.combo_date').hasClass('clicked')) {
+    if ($('span.combo_date').hasClass('clicked')) {
+
+      var id   = $('span.combo_date.clicked select').attr('id');
+      var kind = (id.indexOf("end") != -1) ? "end" : "start";
+
       $('span.combo_date').not(event.target).removeClass('clicked');
-      if (hasChanged) {
+      console.log("Checking - " + kind);
+
+      if (hasChanged[kind] && allSelected(kind)) {
         $('form').closest('form').submit();
-        hasChanged = false;
+        hasChanged[kind] = false;
       }
     }
   });
 
   $(".chzn-select").change(function() {
-    $(this).closest('form').submit();
+     $(this).closest('form').submit();
   });
 
   $('select').change(function(){
-    hasChanged = true;
+
+    var id = $(this).attr('id');
+    var kind = (id.indexOf("end") != -1) ? "end" : "start";
+
+    if (allSelected(kind)) {
+
+      var m    = parseInt($("select#search_when_"+kind+"_2i option:selected").val());
+      if (m < 10) m = "0" + m;
+
+      var day  =  parseInt($("#search_when_"+kind+"_3i_chzn span").html());
+      if (day < 10) day = "0" + day;
+
+      var year =  $("#search_when_"+kind+"_1i_chzn span").html();
+
+      var date = m + "/" + day + "/" + year;
+
+      $(this).parents(".combo_date").find("p:eq(0)").html(date);
+    }
+
+    console.log('Setting ' + kind);
+    hasChanged[kind] = true;
+
   });
 
   $('.changes_list .change .more a').live('click', function(evt){
@@ -58,6 +106,5 @@ $(document).ready(function(ev){
       change.find('.detail').slideToggle();
     }
   });
-
 });
 
