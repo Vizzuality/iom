dfcaa8637dd0c6800af2be852ef123e1d1a7aba1
Fernando Espinosa <fer@ferdev.com>
2012-04-25 13:45:35 +0200

    Shuffles images in main gallery and activates autoplay

--- public/javascripts/sites/home_sites.js
+++ public/javascripts/sites/home_sites.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 
    if ($('div.content_gallery').length>0){
-     $('div.content_gallery').galleria({thumbnails:false, preload:2,autoplay:false,transition:'fade',show_counter:'false'});
+     $('div.content_gallery').galleria({thumbnails:false, preload:2, autoplay:true, transition:'fade', show_counter:'false'});
    }
 
 });
