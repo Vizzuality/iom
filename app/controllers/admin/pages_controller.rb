c75e87931a11566b95b8d6acfd6ed9f2d61a7b95
Fernando Espinosa <fer@ferdev.com>
2012-05-04 14:18:44 +0200

    Allows to admin static pages for the main site

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,48 +1,62 @@
 class Admin::PagesController < ApplicationController
+  include Admin::PagesHelper
 
   before_filter :login_required, :set_site
 
   def index
-    @pages = @site.pages.highlighted.paginate :per_page => 20, :order => 'created_at DESC', :page => params[:page]
+    @pages = pages.highlighted.paginate :per_page => 20, :order => 'created_at DESC', :page => params[:page]
+    @all_pages = @pages
   end
 
   def new
-    @page = @site.pages.new
+    @page = pages.new
+    @all_pages = pages.highlighted
   end
 
   def create
-    @page = @site.pages.new(params[:page])
+    @pages = pages.highlighted
+    @page = pages.new(params[:page])
     if @page.save
-      redirect_to edit_admin_site_page_path(@site, @page), :flash => {:success => 'Page has been created successfully'}
+      redirect_to edit_page_path_for_home_or_site(@page), :flash => {:success => 'Page has been created successfully'}
     else
       render :action => 'new'
     end
   end
 
   def edit
-    @page = @site.pages.from_param(params[:id])
+    @all_pages = pages.highlighted
+    @page = pages.from_param(params[:id])
   end
 
   def update
-    @page = @site.pages.find_by_permalink(params[:id])
+    @page = pages.find_by_permalink(params[:id])
 
     if @page.update_attributes(params[:page])
-      redirect_to edit_admin_site_page_path(@site, @page), :flash => {:success => 'Page has been updated successfully'}
+      redirect_to edit_page_path_for_home_or_site(@page), :flash => {:success => 'Page has been updated successfully'}
     else
       render :action => 'edit'
     end
   end
 
   def destroy
-    @page = @site.pages.find(params[:id])
+    @page = pages.from_param(params[:id])
     @page.destroy
-    redirect_to admin_site_pages_path(@site), :flash => {:success => 'Page has been deleted successfully'}
+    redirect_to pages_path_for_home_or_site, :flash => {:success => 'Page has been deleted successfully'}
+  end
+
+  private
+  def pages
+    pages = if @site.present?
+              @site.pages
+            else
+              MainPage
+            end
   end
 
   protected
 
     def set_site
-      @site = Site.find(params[:site_id])
+      @site = Site.find(params[:site_id]) if params[:site_id]
     end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,24 @@
 class PagesController < ApplicationController
+  skip_before_filter :set_site
+  before_filter :set_site_if_exists
 
-  layout 'site_layout'
+  layout :sites_layout
 
   def show
-    unless @page = @site.pages.find_by_permalink(params[:id])
+    unless @page = pages.find_by_permalink(params[:id])
       raise ActiveRecord::RecordNotFound
     end
   end
 
+  def set_site_if_exists
+    set_site rescue nil
+  end
+  private :set_site_if_exists
+
+  def pages
+    return @site.pages if @site.present?
+    @pages = MainPage.order('order_index asc').all
+    MainPage
+  end
+  private :pages
 end
