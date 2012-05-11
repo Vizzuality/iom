c63c6edede2bda213ca8f0d9d74733180e5fc419
Fernando Espinosa <fer@ferdev.com>
2012-05-11 12:22:18 +0200

    Enables autoplay in home gallery

--- public/javascripts/sites/home_sites.js
+++ public/javascripts/sites/home_sites.js
@@ -3,6 +3,6 @@ $(document).ready( function() {
 
    if ($('div.content_gallery').length>0){
        Galleria.loadTheme('/javascripts/plugins/galleria.home.js');
-       $('div.content_gallery').galleria({thumbnails:false, preload:2,autoplay:false,transition:'fade',show_counter:'false'});
+       $('div.content_gallery').galleria({thumbnails:false, preload:2,autoplay:true,transition:'fade',show_counter:'false'});
    }
 });
