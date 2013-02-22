539b99413ea0c9b0fe875070d3f2d72b7c8f00a7
Fernando Espinosa <fer@ferdev.com>
2013-02-22 10:28:10 +0100

    Allows to see the rest of donors in a region detail

--- app/models/region.rb
+++ app/models/region.rb
@@ -86,13 +86,17 @@ class Region < ActiveRecord::Base
     ).first['count'].to_i
   end
 
-  def donors(site, limit = 11)
+  def donors(site, limit = nil)
+    limit = ''
+    limit = "LIMIT #{limit}" if limit.present?
+
     sql="select distinct don.* from projects_sites as ps
     inner join donations as d on ps.project_id=d.project_id and ps.site_id=#{site.id}
     inner join projects as p on d.project_id = p.id and (p.end_date is null OR p.end_date > now())
     inner join donors as don on don.id=d.donor_id
     inner join projects_regions as pr on ps.project_id=pr.project_id and region_id=#{self.id}
-    limit #{limit}"
+    #{limit}"
+
     Donor.find_by_sql(sql)
   end
 
